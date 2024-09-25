def2a8f6445a8f1752c70cc6974f572381e688e9
Umair Jaffar <umairjaffar.dev@gmail.com>
2024-09-25 17:58:05 +0500

    chaiids

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,34 @@
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import EditInvoiceForm from "@/app/ui/invoices/edit-form";
+import { notFound } from "next/navigation";
+
+const UpdateInvoicePage = async ({ params }: { params: { id: string } }) => {
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) {
+    notFound();
+  }
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <EditInvoiceForm invoice={invoice} customers={customers} />
+    </main>
+  );
+};
+
+export default UpdateInvoicePage;
